1bdc6c868c758df96e9f66f193f2d0a06a29e7fa
Drew Pereli <drewpereli@gmail.com>
2020-12-11 10:31:38 -0800

    fixup! Add Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,24 @@ class UsersController < ApplicationController
       token = encode_token({user_id: @user.id})
       render json: {user: @user, token: token}
     else
-      render json: {error: "Invalid username or password"}
+      render json: {error: "Invalid email or password"}
     end
   end
 
   def login
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
       token = encode_token({user_id: @user.id})
       render json: {user: @user, token: token}
     else
-      render json: {error: "Invalid username or password"}
+      render json: {error: "Invalid email or password"}
     end
   end
 
   private
 
   def user_params
-    params.permit(:username, :password)
+    params.permit(:email, :password)
   end
 end
